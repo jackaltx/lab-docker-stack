1563ce1c51ee7c4eb014455b004ecab4f21d4b54
jackal <jack@lavnet.net>
2025-12-12 10:41:12 -0600

    correcting minio build

--- minio/build_stack_data.sh
+++ minio/build_stack_data.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 source .env
-mkdir -p "${DOCKER_ROOT}/Stacks/redis"
-mkdir -p "${DOCKER_ROOT}/Stacks/redis-insight"
-mkdir -p "${DOCKER_ROOT}/Stacks/redis"
+mkdir -p "${DOCKER_ROOT}/Stacks/minio""
