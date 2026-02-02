126936e5ac4722ed37c7e5319f59c0648a5e5db0
jackal <jack@lavnet.net>
2026-02-02 12:05:26 -0600

    conduit: add config file support

--- conduit/build_stack_data.sh
+++ conduit/build_stack_data.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 source .env
 mkdir -p "${DOCKER_ROOT}/Stacks/conduit/db"
+
+# Generate conduit.toml with actual values
+sed "s/CONDUIT_HOST_PLACEHOLDER/${CONDUIT_HOST}/g; s/DOMAIN_PLACEHOLDER/${DOMAIN}/g" \
+    conduit.toml > "${DOCKER_ROOT}/Stacks/conduit/conduit.toml"
+
+echo "Generated ${DOCKER_ROOT}/Stacks/conduit/conduit.toml with server_name=${CONDUIT_HOST}.${DOMAIN}"
